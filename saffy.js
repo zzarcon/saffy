225bed69f928a9361718d0e7ccfbb70f5671796a
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-28 22:33:10 +0100

    Implement Get

--- saffy.js
+++ saffy.js
@@ -1,5 +1,24 @@
-var get = () => {
-  console.log('get')
+/**
+ * TODO: Check if arguments are valid.
+ *  
+ * @param  {Object} obj     
+ * @param  {String} keyName 
+ * @return {Mixed}         
+ */
+var get = (obj, keyName) => {
+  if (arguments.length < 2 || typeof keyName !== 'string') return;
+
+  let properties = keyName.split('.');
+  
+  if (properties.length === 1) {
+    return obj[keyName];
+  }
+
+  let firstProp = properties.shift();
+  let nextObj = obj[firstProp];
+  let nextProps = properties.join('.');
+
+  return nextObj ? get(nextObj, nextProps) : undefined;
 };
 
 var set = () => {
--- test/specs.js
+++ test/specs.js
@@ -6,16 +6,29 @@ describe('Saffy', function() {
   var dummy = {
     foo: 1,
     user: {
+      city: 'Valencia',
       name: {
         firstName: 'hector',
         lastName: 'zarco'
       }
     }
   };
-  
+
   describe('Get', function() {
     it('Should exist', function() {
       expect(saffy.get).to.be.a('function');
+    });
+
+    it('Returns value of the passed property', function() {
+      expect(saffy.get(dummy, 'foo')).to.equal(dummy.foo);
+      expect(saffy.get(dummy, 'user.city')).to.equal(dummy.user.city);
+      expect(saffy.get(dummy, 'user.name.firstName')).to.equal(dummy.user.name.firstName);
+    });
+
+    it("Returns undefined if the property doesn't exist", function() {
+      expect(saffy.get(dummy, 'bar')).to.be.undefined;
+      expect(saffy.get(dummy, 'user.country')).to.be.undefined;
+      expect(saffy.get(dummy, 'bar.country')).to.be.undefined;
     });
   });
 
